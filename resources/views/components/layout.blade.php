2fec5e78c4399babb139a3e4f1943e9fde784d7b
Imon <contact@imonweb.co.uk>
2024-09-04 01:15:45 +0100

    migration

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Laravel App</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css">
+</head>
+<body>
+    
+    {{ $slot }}
+</body>
+</html>
